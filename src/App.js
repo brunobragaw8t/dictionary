76475a9e21c38dbcfc58a418ef1af5e69acb7200
Bruno Braga <brunobragaw8t@gmail.com>
2021-12-15 08:27:06 +0000

    Install Axios and test API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import axios from 'axios';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+  const dictionaryApi = async() => {
+    try {
+      const data = await axios.get('https://api.dictionaryapi.dev/api/v2/entries/en/plane');
+
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    dictionaryApi();
+  }, []);
+
   return (
     <div className="App">
-      Hello World
+      Dictionary
     </div>
   );
 }
